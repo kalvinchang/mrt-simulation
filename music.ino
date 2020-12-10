0b610037dac36c950ed697423dc219824a9bdac6
Kalvin Chang <kalvin1204@gmail.com>
2020-12-10 00:29:39 -0800

    encode bannan line song using Tune library

---
+++ music.ino
@@ -0,0 +1,53 @@
+#define E4 329.63
+#define Fsharp4 369.99
+#define A4 440
+#define B4 493.88
+#define C5 523.25
+#define Csharp5 554.37
+#define D5 587.33
+#define E5 659.26
+#define F5 698.46
+#define Fsharp5 739.99
+#define G5 783.99
+#define A5 880
+#define B5 987.77
+#define C6 1046.5
+#define E6 1318.51
+#define A7 3520
+
+// sheet music from https://www.youtube.com/watch?v=n5XrDwuomoE
+void enterStationTune(int speaker) {
+  // each line = 1 measure in 5/4 time
+  double notes[40] = {
+    A5, E5, A5, B5, E6, A7,
+    Csharp5, E5, A4, B4, D5,
+    Csharp5, E5, A4, B4, Fsharp5,
+    Csharp5, E5, A4, B4, Fsharp4,
+    E4,
+    E5, G5, C5, D5, F5,
+    E5, G5, C5, D5, A5,
+    G5, C6, E5, F5, G5,
+    E5, G5, E6
+  };
+
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  double durations[40] = {
+    8, 8, 8, 8, 4, 2,
+    4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4,
+    0.80,
+    4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4,
+    8, 8, 1
+  };
+
+  // adapted from https://www.arduino.cc/en/Tutorial/BuiltInExamples/toneMelody
+  for (int i = 0; i < 40; i++) {
+    double noteDuration = 1000 / durations[i];
+    tone(speaker, notes[i], noteDuration);
+    delay(noteDuration * 1.30);
+    noTone(speaker);
+  }
+}
